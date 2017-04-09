697322030eadafe181dea72431a292aab0a4c442
Kasidech Chumkun <failkawae@gmail.com>
2017-04-09 14:02:33 +0700

    Revert "declare some method"
    This reverts commit 11067e19132596992ad5ef6e265646cad63332ed.

--- Evilhangman/src/evilhangman/Evilhangman.java
+++ Evilhangman/src/evilhangman/Evilhangman.java
@@ -1,82 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package evilhangman;
 
-public class EvilHangman {
+/**
+ *
+ * @author macos
+ */
+public class Evilhangman {
 
-    private String secretWord;
-    private String dashline;
-    private String[] word;
-    private char[] letterGuessHistory;
-    private int secretWordLength;
-    private int guessCount;
-    private int pointer_guesshistory;
-    private boolean guessResult;
-
-    public static void main(String[] arg) {
-        EvilHangman e1 = new EvilHangman();
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
     }
-
-    public EvilHangman() {
-
-    }
-
-    public String getSecretWord() {
-        return secretWord;
-    }
-
-    public void setSecretWord(String secretWord) {
-        this.secretWord = secretWord;
-    }
-
-    public int getSecretWordLength() {
-        return secretWordLength;
-    }
-
-    public void setSecretWordLength(int secretWordLength) {
-        this.secretWordLength = secretWordLength;
-    }
-
-    public String displayDashline() {
-        return dashline;
-    }
-
-    public int getGuessCount() {
-        return guessCount;
-    }
-
-    public void setGuessCount(int guessCount) {
-        this.guessCount = guessCount;
-    }
-
-    public int getPointer_guesshistory() {
-        return pointer_guesshistory;
-    }
-
-    public void setPointer_guesshistory(int pointer_guesshistory) {
-        this.pointer_guesshistory = pointer_guesshistory;
-    }
-
-    public boolean getGuessResult() {
-        return guessResult;
-    }
-
-    public void setGuessResult(boolean guessResult) {
-        this.guessResult = guessResult;
-    }
-
-    public boolean chooseWord(char ch) {
-        return false;
-    } // nothing
-
-    public char[] addLetterGuess(char ch) { // nothing
-        return letterGuessHistory;
-    }
-
-    public boolean isSameGuessLetter() { // nothing
-        return false;
-    }
-
-    public boolean guessLetter() { //nothing
-        return false;
-    }
-
+    
 }
