1be9ba79bd156425a9f3e4fa1fcadd25c7189067
bonusjatupat <jatupat.bonus@mail.kmutt.ac.th>
2017-04-20 00:36:15 +0700

    added checkSecretWord() method

--- EvilHangman_CSC105/src/evilhangman_csc105/EvilHangman_CSC105.java
+++ EvilHangman_CSC105/src/evilhangman_csc105/EvilHangman_CSC105.java
@@ -14,13 +14,15 @@ public class EvilHangman_CSC105 {
     private String secretWord;
     private String dashline;
     private String[] word = new String[50000];
-    private char[] letterGuessHistory = new char[10];
+    private char[] letterGuessHistory = new char[20];
     private int secretWordLength;
     private int guessCount;
     private int pointer_guesshistory;
     private boolean guessResult;
     
     private static int countLetterGuessHistory;
+    private static int countTotalWord;
+    private boolean guessLetter;
 
     public static void main(String[] arg) {
         EvilHangman_CSC105 e1 = new EvilHangman_CSC105();
@@ -28,14 +30,19 @@ public class EvilHangman_CSC105 {
         System.out.println(e1.displayDashline());
         e1.getWordsFromDic();
         e1.inputLetter();
-        e1.inputLetter();
-        e1.inputLetter();
-        e1.inputLetter();
+        e1.checkSecretWord();
     }
 
     public EvilHangman_CSC105() {
         guessCount = 10;
         countLetterGuessHistory = 0;
+        countTotalWord = 0;
+        countWord0 = 0;
+        countWord1 = 0;
+        countWord2 = 0;
+        countWord3 = 0;
+        countWord4 = 0;
+        countWord5 = 0;
     }
 
     public void inputLetter(){
@@ -68,18 +75,17 @@ public class EvilHangman_CSC105 {
     public void getWordsFromDic(){
         String fileName = "C:\\Users\\PS\\Desktop\\EvilHangman_CSC105\\dictionary.txt";
         File file = new File(fileName);
-        int count=0;
         try {
             BufferedReader br = new BufferedReader(new FileReader(file));
             String line;
             while((line = br.readLine()) != null){
                 if(line.length() == secretWordLength){ //เลือกเฉพาะคำที่มีlegthตามต้องการ
-                    word[count] = line; //arrayคำที่มีจำนวนlengthที่กำหนด
+                    word[countTotalWord] = line; //arrayคำที่มีจำนวนlengthที่กำหนด
                     //System.out.println(word[count]); //ลองปริ้น
-                    count++;
+                    countTotalWord++;
                 }
             }
-            System.out.println("Words with length " + secretWordLength + " have " + count + " words");
+            System.out.println("Words with length " + secretWordLength + " have " + countTotalWord + " words");
         } catch (FileNotFoundException ex) {
             Logger.getLogger(EvilHangman_CSC105.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
@@ -87,7 +93,57 @@ public class EvilHangman_CSC105 {
         }
     }
     
-    public String getSecretWord() {
+    private String[] word0 = new String[50000];
+    private int countWord0;
+    private String[] word1 = new String[50000];
+    private int countWord1;
+    private String[] word2 = new String[50000];
+    private int countWord2;
+    private String[] word3 = new String[50000];
+    private int countWord3;
+    private String[] word4 = new String[50000];
+    private int countWord4; 
+    private String[] word5 = new String[50000];
+    private int countWord5;
+    public void checkSecretWord(){ //group the words 
+        for(int i=0; i<countTotalWord; i++){ //check every word
+            int countHaveLetter = 0;
+            for(int j=0; j<secretWordLength; j++){ //check every leter
+                char temp = word[i].charAt(j);
+                if(letterGuessHistory[countLetterGuessHistory-1] == temp){
+                    countHaveLetter++; //count letters in a word
+                }
+            }
+            if(countHaveLetter == 0){ //separate words into groups
+                word0[countWord0] = word[i]; 
+                //System.out.println(word0[countWord0]);
+                countWord0++;
+            } else if(countHaveLetter == 1){
+                word1[countWord1] = word[i];
+                countWord1++;
+            } else if(countHaveLetter == 2){
+                word2[countWord2] = word[i];
+                countWord2++;
+            } else if(countHaveLetter == 3){
+                word3[countWord3] = word[i];
+                countWord3++;
+            } else if(countHaveLetter == 4){
+                word4[countWord4] = word[i];
+                countWord4++;
+            } else if(countHaveLetter == 5){
+                word5[countWord5] = word[i];
+                countWord5++;
+            }
+        }
+        System.out.println("0 letter have : "+countWord0+" words");
+        System.out.println("1 letter have : "+countWord1+" words");
+        System.out.println("2 letter have : "+countWord2+" words");
+        System.out.println("3 letter have : "+countWord3+" words");
+        System.out.println("4 letter have : "+countWord4+" words");
+        System.out.println("5 letter have : "+countWord5+" words");   
+    }
+    
+    public String getSecretWord() {    
         return secretWord;
     }
     
